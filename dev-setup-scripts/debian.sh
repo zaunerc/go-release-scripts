bd70c4928ab96f1d08d066e74afa0fb5e45747a0
Christoph Zauner <christoph.zauner@NLLK.net>
2016-09-30 21:58:23 +0200

    Under Debian we require the vim-nox package.

--- dev-setup-scripts/debian.sh
+++ dev-setup-scripts/debian.sh
@@ -3,7 +3,12 @@
 set -e
 
 sudo aptitude update
-sudo aptitude install git golang-any exuberant-ctags glide curl jq
+
+# The Vim neocomplete plugin requires Lua support.
+# The vim-nox package provides Vim with Lua support
+# compiled in.
+#
+sudo aptitude install git golang-any exuberant-ctags glide curl jq vim-nox
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 $SCRIPT_DIR/common.sh
