d585607e20c0f65d80b498a36f258e5a773b2496
Christoph Zauner <christoph.zauner@NLLK.net>
2016-10-05 05:57:59 +0000

    Add required packages for GoRename.

--- dev-setup-scripts/alpine.sh
+++ dev-setup-scripts/alpine.sh
@@ -3,7 +3,18 @@
 set -e
 
 echo '@edge-community http://nl.alpinelinux.org/alpine/edge/community' | sudo tee -a /etc/apk/repositories > /dev/null
-sudo apk add --update --no-cache git go ctags glide@edge-community curl jq
+sudo apk add --update --no-cache \
+	# Required by GoRename
+	gcc \
+	musl-dev \
+	# Required by https://github.com/zaunerc/go-scripts/tree/master/release-scripts 
+	curl \
+	jq \
+	# Misc
+	ctags \
+	git \
+	glide@edge-community \
+	go 
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 $SCRIPT_DIR/common.sh
