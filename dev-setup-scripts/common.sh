2c26c101dce96ab69b36756b40a03967888504be
Christoph Zauner <christoph.zauner@NLLK.net>
2016-10-27 14:01:58 +0200

    Run Vundle plugin installation in VIM EX mode.

--- dev-setup-scripts/common.sh
+++ dev-setup-scripts/common.sh
@@ -29,7 +29,10 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
-vim +PluginInstall +GoInstallBinaries +qall
+# Vundle plugin installation in headless, non-interactive mode.
+# Any errors are ignored.
+#
+vim -E -s -c "source ~/.vimrc" -c PluginInstall -c qa
 
 cat << EOF
 
